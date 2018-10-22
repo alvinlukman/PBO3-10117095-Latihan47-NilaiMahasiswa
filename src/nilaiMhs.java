70206fd7d66102edbb66ff2b8f7ffdf1d036493b
alvinlukmanNH <galinx20gmail.com>
2018-10-22 23:22:20 +0700

    nilaimhs

---
+++ src/nilaiMhs.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 
+ * NAMA  :Alvin Lukman Nulhakim
+ * NIM   :10117095    
+ * KELAS :IF-3
+ */
+public class nilaiMhs {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Index objI = new Index();
+        objI.setQuis(75.0);
+        objI.setUts(45.0);
+        objI.setUas(34.0);
+        System.out.println("QUIZ\t= "+objI.getQuis());
+        System.out.println("UTS\t= "+objI.getUts());
+        System.out.println("UAS\t= "+objI.getUas());
+        System.out.println("");
+        System.out.println("Nilai Akhir = "+objI.nilaiAkhir(0));
+        System.out.println("");
+        System.out.println("");
+        
+        
+        Index obj2 = new Index();
+        obj2.setQuis(60.0);
+        obj2.setUts(80.0);
+        obj2.setUas(75.0);
+        System.out.println("QUIZ\t= "+obj2.getQuis());
+        System.out.println("UTS\t= "+obj2.getUts());
+        System.out.println("UAS\t= "+obj2.getUas());
+        System.out.println("");
+        System.out.println("Nilai Akhir = "+obj2.nilaiAkhir(0));
+        System.out.println("");
+        System.out.println("");
+        
+        
+        Index obj3 = new Index();
+        obj3.setQuis(30.0);
+        obj3.setUts(80.0);
+        obj3.setUas(40.0);
+        System.out.println("QUIZ\t= "+obj3.getQuis());
+        System.out.println("UTS\t= "+obj3.getUts());
+        System.out.println("UAS\t= "+obj3.getUas());
+        System.out.println("");
+        System.out.println("Nilai Akhir = "+obj3.nilaiAkhir(0));
+        System.out.println("");
+        System.out.println("");
+
+    }
+    
+}
